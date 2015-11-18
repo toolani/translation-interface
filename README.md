d888f2f465c0ba8a73d7e8b9ae1be9e56d9e4d5d
pthompson <peter.thompson@toolani.com>
2015-11-18 10:53:55 +0100

    Add a local node web server
    Allows access to a local version of the translation-api via the web UI.

--- README.md
+++ README.md
@@ -8,14 +8,26 @@ A web interface for interacting with [go-translation-api][1].
 
 You will need [npm][npm] and [grunt][grunt] installed to be able to build the project.
 
-The API provided by [go-translation-api][1]'s `serve` command must be available on the same domain you intend to run this interface on under the `/translation-api/` path. i.e. If you are installing to `www.yourserver.com`, the api server must be reachable at `www.yourserver.com/translation-api/`.
+You will also need a compiled version of the [go-translation-api][1].
 
-## Installation
+## Installation on an existing web server
+
+The API provided by go-translation-api's `serve` command must be available on the same domain you intend to run this interface on under the `/translation-api/` path. i.e. If you are installing to `www.yourserver.com`, the api server must be reachable at `www.yourserver.com/translation-api/`.
 
 1. Clone this project.
 2. Run `npm install` in the project's root directory.
 3. Run `grunt` in the project's root directory.
 4. Upload the content of the `web/` directory to your webserver.
+
+## Local installation
+
+A nodejs web server is provided for testing the translation interface.
+
+1. Clone this project.
+2. Run `npm install` in the project's root directory.
+3. Run `grunt` in the project's root directory.
+4. Ensure the go-translation-api's `serve` command is running on your local machine on port 8181.
+5. Run `npm run serve` from the project's root directory. This will start a web server on port 8080 providing access to the translation interface. Simply access http://localhost:8080/ in your web browser to get started.
 
 [1]:https://github.com/petert82/go-translation-api
 [npm]: https://www.npmjs.com/
---
+++ server/serve.js
@@ -0,0 +1,88 @@
+var fs = require('fs')
+var http = require('http')
+var httpProxy = require('http-proxy')
+var moment = require('moment')
+var path = require('path')
+var url = require('url')
+var baseDirectory =  path.resolve(__dirname, path.join('..', 'web'))
+
+// HTTP server will listen on this port
+var webPort = 8080
+// The React app expects the translation JSON API to be available under this path.
+var apiRoot = '/translation-api'
+
+// Will proxy requests to the JSON API
+var apiProxy = httpProxy.createProxyServer({target:'http://localhost:8181'})
+
+// Rewrite requests to remove the apiRoot from the path.
+apiProxy.on('proxyReq', function(proxyReq) {
+  if (proxyReq.path === apiRoot) {
+    proxyReq.path = '/'
+  } else {
+    proxyReq.path = proxyReq.path.substr(apiRoot.length)
+  }
+});
+
+apiProxy.on('proxyRes', function(proxyRes, req) {
+    logRequest(req, proxyRes)
+})
+
+apiProxy.on('error', function (err, req, res) {
+  res.writeHead(500);
+  res.end();
+  logRequest(req, res)
+});
+
+// Proxy API requests to the API, or otherwise, try to serve the requested file from the web/ 
+// subdirectory
+http.createServer(function(req, res) {
+    var requestUrl = url.parse(req.url)
+    var apiRegex = new RegExp('^' + apiRoot)
+    
+    try {
+        if (apiRegex.test(requestUrl.pathname)) {
+            apiProxy.web(req, res)
+        } else {
+            serveFile(req, res)
+        }
+    } catch(e) {
+        if (!res.finished) {
+            res.writeHead(500)
+            res.end()
+        }
+        logRequest(req, res)
+    }
+}).listen(webPort)
+
+console.log('Listening on port', webPort)
+
+var serveFile = function(req, res) {
+    var requestUrl = url.parse(req.url)
+    var fsPath = baseDirectory + requestUrl.pathname
+    
+    if (requestUrl.pathname === '/') {
+        fsPath += 'index.html'
+    }
+    
+    fs.exists(fsPath, function(exists) {
+        if (exists) {
+            res.writeHead(200)
+            var responseStream = fs.createReadStream(fsPath).pipe(res)
+            responseStream.on('finish', function () { 
+                res.end()
+                logRequest(req, res)
+            });
+        } else {
+            res.writeHead(404)
+            res.end()
+            logRequest(req, res)
+        }
+    })
+}
+
+var logRequest = function (req, res) {
+    var requestUrl = url.parse(req.url)
+    var date = moment().format('DD/MMM/YYYY:HH:mm:ss ZZ')
+    var logLine = req.socket.remoteAddress + ' [' + date + ']  "' + req.method + ' ' + requestUrl.pathname + '" ' + res.statusCode
+    console.log(logLine)
+}
