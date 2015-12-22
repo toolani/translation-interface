31eb2019f8abcdf8814458460df69315f4f700f3
pthompson <peter.thompson@toolani.com>
2015-12-22 13:08:16 +0100

    Update to latest React

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,14 @@
 import 'babel-polyfill'
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
 import App from './containers/App'
 
 let store = configureStore()
 
-React.render((
+ReactDOM.render((
     <Provider store={store}>
-        {() => <App />}
+        <App />
     </Provider>
 ), document.getElementById('app-container'))
